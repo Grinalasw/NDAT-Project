547f45ac41937b49625475418bd329e26c0a3b9f
Grinalasw <frontendasw@gmail.com>
2025-11-18 16:01:11 +0530

    design tabbing

--- index.php
+++ index.php
@@ -98,6 +98,149 @@
     </div>
 </section>
 
+<section class="program-career-section section-gap double-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="pc-wrap">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item w-100">
+                            <a class="nav-link active text-center" data-bs-toggle="pill" href="#program_highlights">
+                                Program <span>Highlights</span>
+                            </a>
+                        </li>
+                        <li class="nav-item w-100">
+                            <a class="nav-link text-center" data-bs-toggle="pill" href="#career_opportunities">
+                                Career <span>Opportunities</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="program_highlights" class="tab-pane active">
+                            <div class="pc-list program-highlights">
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-school"></i>
+                                    <p class="pc-para">
+                                        Ranked 4th Best Design School by Career 360
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-cap"></i>
+                                    <p class="pc-para">
+                                        One-of-a-kind Undergraduate Design Program in India
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-cup"></i>
+                                    <p class="pc-para">
+                                        10+ Awards & 5 Finalists at National & International forums
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-free-network"></i>
+                                    <p class="pc-para">
+                                        Strong Alumni Network across industries
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-network"></i>
+                                    <p class="pc-para">
+                                        100% Placement for three consecutive years
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-users"></i>
+                                    <p class="pc-para">
+                                        5-Month Paid Internship with leading organizations
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-book"></i>
+                                    <p class="pc-para">
+                                        Small Batch Size for personalized mentorship
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-heart"></i>
+                                    <p class="pc-para">
+                                        Immersive NGO Projects for real-world exposure
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-global"></i>
+                                    <p class="pc-para">
+                                        Hands-on Learning through studios & labs
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-strem"></i>
+                                    <p class="pc-para">
+                                        Entrepreneurial Mentorship to nurture future founders
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-chalkboard"></i>
+                                    <p class="pc-para">
+                                        Expert Faculty from academia and industry
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-bookopen"></i>
+                                    <p class="pc-para">
+                                        26+ Research Papers published in international journals & conferences
+                                    </p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div id="career_opportunities" class="tab-pane fade">
+                            <p class="pc-title">Graduates of the B.Des. (Humanising Technology) can look forward to diverse career opportunities, including:</p>
+                            <div class="pc-list career-opportunities">
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-idesigner"></i>
+                                    <p class="pc-para">
+                                        Interaction Designer
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-dresearcher"></i>
+                                    <p class="pc-para">
+                                        Design Researcher
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-datavisual"></i>
+                                    <p class="pc-para">
+                                        Data Visualizer
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-uiux"></i>
+                                    <p class="pc-para">
+                                        UI & UX Designer
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-edesigner"></i>
+                                    <p class="pc-para">
+                                        Experience Designer Emerging Technology
+                                    </p>
+                                </div>
+                                <div class="pc-item">
+                                    <i class="n-icon pc-icon" data-icon="i-graphicdesign"></i>
+                                    <p class="pc-para">
+                                        Graphic Designer
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php include 'Template/footer-main.php'; ?>
 <?php include 'Template/footer.php'; ?>
